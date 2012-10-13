209ec0e8ad8fb19399d051302f144d7fcf0c5499
Kirill Nikitin <locke23rus@gmail.com>
2012-10-13 17:53:41 +0400

    Add validations to recommendations.

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -3,4 +3,8 @@ class Recommendation < ActiveRecord::Base
   belongs_to :user
   belongs_to :repo
 
+  validates :user_id, :presence => true, :uniqueness => { :scope => :user_id }
+  validates :repo_id, :presence => true
+
+
 end
