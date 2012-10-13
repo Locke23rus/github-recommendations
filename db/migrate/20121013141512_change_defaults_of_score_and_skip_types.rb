1e6dfef1658d0c3b0e371d7d33fe16d101699201
Kirill Nikitin <locke23rus@gmail.com>
2012-10-13 18:24:00 +0400

    Fix bad migration.

--- db/migrate/20121013141512_change_defaults_of_score_and_skip_types.rb
+++ db/migrate/20121013141512_change_defaults_of_score_and_skip_types.rb
@@ -1,6 +1,11 @@
 class ChangeDefaultsOfScoreAndSkipTypes < ActiveRecord::Migration
-  def change
+  def up
     change_column :recommendations, :score, :integer, :default => 0
     change_column :recommendations, :skip_type, :integer, :default => 0
+  end
+
+  def down
+    change_column :recommendations, :score, :integer
+    change_column :recommendations, :skip_type, :integer
   end
 end
