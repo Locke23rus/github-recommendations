5226d47e9f3690f728906c62cd876f96c9f4de93
Kirill Nikitin <locke23rus@gmail.com>
2012-10-13 18:13:28 +0400

    Add association scores to recommendations. Add skip types to recommendations.

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -2,8 +2,14 @@ class Recommendation < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :repo
+  has_many :scores, :dependent => :delete_all
 
   validates :user_id, :presence => true, :uniqueness => { :scope => :repo_id }
   validates :repo_id, :presence => true
+
+  SKIP_TYPES = {
+    :user => 1,
+    :auto => 2
+  }
 
 end
