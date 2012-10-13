59503128becc54c6f5b91931542abb264cfe113e
Shaliko Usubov <shaliko@ezid.ru>
2012-10-13 12:33:31 +0400

    Rename project

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Githubrec::Application.configure do
+Githubrecomentation::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,4 @@
-Githubrec::Application.configure do
+Githubrecomentation::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # The test environment is used exclusively to run your application's
