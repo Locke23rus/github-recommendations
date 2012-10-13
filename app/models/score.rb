7dc9bf4e63e07ab6ff429791d7e71aa5a063a565
Kirill Nikitin <locke23rus@gmail.com>
2012-10-14 01:41:56 +0400

    Add action types and names

--- app/models/score.rb
+++ app/models/score.rb
@@ -11,4 +11,17 @@ class Score < ActiveRecord::Base
     self.value = 1
   end
 
+  ACTION_NAMES = {
+    0 => 'owner',
+    1 => 'collaborators',
+    2 => 'forks',
+    3 => 'starred'
+  }
+
+  ACTION_TYPES = ACTION_NAMES.invert.symbolize_keys!
+
+  def action_name
+    ACTION_NAMES[action_type]
+  end
+
 end
