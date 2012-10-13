b0490411d79243d12694eb16409aa55f2f6555a1
Shaliko Usubov <shaliko@ezid.ru>
2012-10-13 10:02:25 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Githubrec::Application.config.secret_token = 'b986570318841e8aec674fed0d32439cc6acf8d8c58458e978997bdbdbfe9d462a8f63f1d5d09f8d351c1cbf2f0b135d54d55c7d5cf7bfcb42f2766d5e5cd647'
