ef5a0f6b32837174f62a370355fa9e1d19cbb8b1
Shaliko Usubov <shaliko@ezid.ru>
2012-10-15 01:12:42 +0400

    Fetch recommendation by special user

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,7 +1,8 @@
 class RecommendationsController < ApplicationController
+  before_filter :find_user, :only => [:index]
 
   def index
-    @recommendations = current_user.recommendations.available.paginate(:page => params[:page])
+    @recommendations = @user.recommendations.available.paginate(:page => params[:page])
   end
 
   def skip
@@ -10,4 +11,15 @@ class RecommendationsController < ApplicationController
 
     render json: { status: 'OK' }
   end
+
+  private
+
+  def find_user
+    @user = User.enabled.find_by_login(params[:user_id])
+
+    unless @user
+      redirect_to root_path, alert: "User not found."
+    end
+  end
+
 end
