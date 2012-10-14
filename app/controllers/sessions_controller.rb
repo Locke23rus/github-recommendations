74a5aad6863228882c9a6e1acc7a206e28daf7ef
Kirill Nikitin <locke23rus@gmail.com>
2012-10-14 20:53:47 +0400

    Small tweaks

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
     auth = request.env['omniauth.auth']
     user = User.find_or_update_with_omniauth!(auth) || User.create_with_omniauth!(auth)
     session[:user_id] = user.id
-    redirect_to user_path(user), notice: "Signed in."
+    redirect_to user_path(user.login), notice: "Signed in."
   end
 
   def destroy
